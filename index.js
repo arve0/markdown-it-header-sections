e6fe88f3ddee54cf6960f48dc19687d65bcdae7a
arve0 <arve.seljebu@gmail.com>
2015-07-20 13:07:48 +0200

    fix only closing one level when stepping down from h3->h1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports = function headerSections(md){
   function addSections(state){
     var tokens = [];  // output
     var Token = state.Token;
+    var sectionLevel = 0;
+    var prevHeaderLevel = 0;
 
     function closeSection(){
       var t = new Token('section_close', 'section', -1);
@@ -18,27 +20,32 @@ module.exports = function headerSections(md){
       return t;
     }
 
-    var currentLevel = 0;
+    function closeSections(from, to) {
+      // close one below to
+      while (from !== (to-1)) {
+        tokens.push(closeSection());
+        from--;
+      }
+      return from;
+    }
+
     for (var i = 0, l = state.tokens.length; i < l; i++) {
       var token = state.tokens[i];
 
       // add sections before headers
       if (token.type == 'heading_open') {
         var level = headingLevel(token.tag);
-        if (level <= currentLevel) {
-          tokens.push(closeSection());
+        if (level <= prevHeaderLevel) {
+          sectionLevel = closeSections(sectionLevel, level);
         }
-        currentLevel = level;
         tokens.push(openSection(token.attrs));
+        sectionLevel++;
+        prevHeaderLevel = level;
       }
 
       tokens.push(token);
     }  // end for every token
-
-    // close sections
-    for (; currentLevel !== 0; currentLevel--) {
-      tokens.push(closeSection());
-    }
+    closeSections(sectionLevel, 1);
 
     state.tokens = tokens;
   }
@@ -46,7 +53,6 @@ module.exports = function headerSections(md){
   md.core.ruler.push('header sections', addSections);
 
 };
-
 
 function headingLevel(header) {
   return parseInt(header.charAt(1));
